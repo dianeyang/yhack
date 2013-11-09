4e47d63092acff3dc59d3b5ce13fdf9498222414
Diane Yang <dianeyang0@gmail.com>
2013-11-08 21:32:39 -0500

    better style

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -1,7 +1,11 @@
-$(document).ready(function() {
-    $('textarea#speech-page-content').bind('input propertychange', $.debounce(250, do_something));
+(function($) {
 
-    function do_something() {
-        console.log('doing something');
-    };
-});
+	$(document).ready(function() {
+	    $('textarea#speech-page-content').bind('input propertychange', $.debounce(250, do_something));
+
+	    function do_something() {
+	        console.log('doing something');
+	    };
+	});
+
+})(jQuery);
