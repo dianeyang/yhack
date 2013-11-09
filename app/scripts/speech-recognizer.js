b15f689f02b5f34e45912bd1fece9e27d84c258a
Diane Yang <dianeyang0@gmail.com>
2013-11-09 03:01:04 -0500

    turning the recording on and off works

--- app/scripts/speech-recognizer.js
+++ app/scripts/speech-recognizer.js
@@ -1,3 +1,60 @@
+// (function($) {
+
+//     $(document).ready(function() {
+
+//         try {
+//             var recognition = new webkitSpeechRecognition();
+//         } catch(e) {
+//             var recognition = Object;
+//         }
+//         recognition.continuous = true;
+//         recognition.interimResults = true;
+
+//         var interimResult = '';
+//         var textArea = $('#speech-page-content');
+//         var textAreaID = 'speech-page-content';
+
+//         $('.cover > i').click(function(){
+//             console.log('clicked');
+//             startRecognition();
+//         });
+
+//         $('.recording').click(function(){
+//             recognition.stop();
+//         });
+
+//         var startRecognition = function() {
+//             //$('.speech-content-mic').removeClass('speech-mic').addClass('speech-mic-works');
+//             console.log('starting recognition')
+//             $('.cover > i').toggleClass('recording');
+//             textArea.focus();
+//             recognition.start();
+//         };
+
+//         recognition.onresult = function (event) {
+//             console.log('result')
+//             var pos = textArea.getCursorPosition() - interimResult.length;
+//             textArea.val(textArea.val().replace(interimResult, ''));
+//             interimResult = '';
+//             textArea.setCursorPosition(pos);
+//             for (var i = event.resultIndex; i < event.results.length; ++i) {
+//                 if (event.results[i].isFinal) {
+//                     insertAtCaret(textAreaID, event.results[i][0].transcript);
+//                 } else {
+//                     isFinished = false;
+//                     insertAtCaret(textAreaID, event.results[i][0].transcript + '\u200B');
+//                     interimResult += event.results[i][0].transcript + '\u200B';
+//                 }
+//             }
+//         };
+
+//         recognition.onend = function() {
+//             //$('.speech-content-mic').removeClass('speech-mic-works').addClass('speech-mic');
+//             $('.microphone').removeClass('recording').addClass('not-recording')
+//         };
+//     });
+// })(jQuery);
+
 (function($) {
 
     $(document).ready(function() {
@@ -14,8 +71,7 @@
         var textArea = $('#speech-page-content');
         var textAreaID = 'speech-page-content';
 
-        $('.cover > i').click(function(){
-            console.log('clicked');
+        $('i.fa-microphone:not(.recording)').click(function(){
             startRecognition();
         });
 
@@ -24,15 +80,12 @@
         });
 
         var startRecognition = function() {
-            //$('.speech-content-mic').removeClass('speech-mic').addClass('speech-mic-works');
-            console.log('starting recognition')
-            $('.cover > i').toggleClass('recording');
+            $('.cover i.fa-microphone').toggleClass('recording');
             textArea.focus();
             recognition.start();
         };
 
         recognition.onresult = function (event) {
-            console.log('result')
             var pos = textArea.getCursorPosition() - interimResult.length;
             textArea.val(textArea.val().replace(interimResult, ''));
             interimResult = '';
@@ -49,8 +102,7 @@
         };
 
         recognition.onend = function() {
-            //$('.speech-content-mic').removeClass('speech-mic-works').addClass('speech-mic');
-            $('.microphone').removeClass('recording').addClass('not-recording')
+            $('.cover i.fa-microphone').toggleClass('recording');
         };
     });
 })(jQuery);
