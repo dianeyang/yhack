a2ea094093b64b31bb264a459c171b74dd2460d8
Colin Lu <colin.h.lu@gmail.com>
2013-11-09 07:03:42 -0500

    clucluclu

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -23,23 +23,146 @@
 	            },
 	            success: function (data) {
 	            	
-	              //  temp.exit().remove()	
-	            	
 	            	console.log(data);
 	            	var len = data.keywords.length;
-	            	var sentiment = 0;
 	            	$("#results").html("");
-	            	$("#sentiment").html("");
-					for (var i = 0; i < len; i++)
-					{
-						sentiment = (1*sentiment)+(1*(data.keywords[i].sentiment.score));
-					}
-					$("#sentiment").append(sentiment);
-				  	for (var i = 0; i < len; i++)
+	            	var myData = new Array;
+	            	var temp = new Array(len);
+	            	myData[0] = 1*data.keywords[0].sentiment.score;
+	            	temp[0] = " ";
+	            	$("#results").append($("<div><strong>"+data.keywords[0].text+"</strong> ("+data.keywords[0].relevance+")</div>"));
+				  	for (var i = 1; i < len; i++)
 					{
 						$("#results").append($("<div><strong>"+data.keywords[i].text+"</strong> ("+data.keywords[i].relevance+")</div>"));
+						myData[i] = (1*myData[i-1])+(1*data.keywords[i].sentiment.score);
+						temp[i] = " ";
 					}
-				  
+				    
+				    $(function () { 
+					    $('#container').highcharts({
+					        chart: {
+					            type: 'line'
+					        },
+					        title: {
+					            text: 'Mood Over Time'
+					        },
+					        xAxis: {
+					            categories: temp
+					        },
+					        yAxis: {
+					            title: {
+					                text: 'Mood'
+					            }
+					        },
+					        series: [{
+					            name: 'Time',
+					            data: myData
+					        }]
+					    });
+					});
+				    /*
+				    var ndata = {
+						labels : temp,
+						datasets : [
+							{
+								fillColor : "rgba(220,220,220,0.5)",
+								strokeColor : "rgba(220,220,220,1)",
+								pointColor : "rgba(220,220,220,1)",
+								pointStrokeColor : "#fff",
+								data : myData
+							}
+						]
+					}
+				    
+				    var options = {
+				
+					//Boolean - If we show the scale above the chart data			
+					scaleOverlay : false,
+					
+					//Boolean - If we want to override with a hard coded scale
+					scaleOverride : false,
+					
+					//** Required if scaleOverride is true **
+					//Number - The number of steps in a hard coded scale
+					scaleSteps : null,
+					//Number - The value jump in the hard coded scale
+					scaleStepWidth : null,
+					//Number - The scale starting value
+					scaleStartValue : null,
+				
+					//String - Colour of the scale line	
+					scaleLineColor : "rgba(0,0,0,.1)",
+					
+					//Number - Pixel width of the scale line	
+					scaleLineWidth : 1,
+				
+					//Boolean - Whether to show labels on the scale	
+					scaleShowLabels : true,
+					
+					//Interpolated JS string - can access value
+					scaleLabel : "<%=value%>",
+					
+					//String - Scale label font declaration for the scale label
+					scaleFontFamily : "'Arial'",
+					
+					//Number - Scale label font size in pixels	
+					scaleFontSize : 12,
+					
+					//String - Scale label font weight style	
+					scaleFontStyle : "normal",
+					
+					//String - Scale label font colour	
+					scaleFontColor : "#666",	
+					
+					///Boolean - Whether grid lines are shown across the chart
+					scaleShowGridLines : true,
+					
+					//String - Colour of the grid lines
+					scaleGridLineColor : "rgba(0,0,0,.05)",
+					
+					//Number - Width of the grid lines
+					scaleGridLineWidth : 1,	
+					
+					//Boolean - Whether the line is curved between points
+					bezierCurve : true,
+					
+					//Boolean - Whether to show a dot for each point
+					pointDot : true,
+					
+					//Number - Radius of each point dot in pixels
+					pointDotRadius : 3,
+					
+					//Number - Pixel width of point dot stroke
+					pointDotStrokeWidth : 1,
+					
+					//Boolean - Whether to show a stroke for datasets
+					datasetStroke : true,
+					
+					//Number - Pixel width of dataset stroke
+					datasetStrokeWidth : 2,
+					
+					//Boolean - Whether to fill the dataset with a colour
+					datasetFill : true,
+					
+					//Boolean - Whether to animate the chart
+					animation : true,
+				
+					//Number - Number of animation steps
+					animationSteps : 60,
+					
+					//String - Animation easing effect
+					animationEasing : "easeOutQuart",
+				
+					//Function - Fires when the animation is complete
+					onAnimationComplete : null
+					
+				}
+				    
+				    
+					var ctx = document.getElementById("myChart").getContext("2d");
+					var myNewChart = new Chart(ctx)
+					new Chart(ctx).Line(ndata);*/
+				    
 				  var fill = d3.scale.category20();
 				  
 				  $("#cloud").html("");
